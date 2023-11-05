9496d3d0a388cb3412029545a761e2b4829bb10f
mohammad azad <mohammadazad.bc@gmail.com>
2023-11-05 12:22:45 +0100

    reviews

--- src/main/java/dev/azad/movies/Review.java
+++ src/main/java/dev/azad/movies/Review.java
@@ -3,6 +3,7 @@ package dev.azad.movies;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,7 +14,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Review {
 
     @Id
-    private Object id;
+    private ObjectId id;
     private String body;
 
     public Review(String body) {
--- src/main/java/dev/azad/movies/ReviewService.java
+++ src/main/java/dev/azad/movies/ReviewService.java
@@ -6,21 +6,27 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Service
 public class ReviewService {
 
-    @Autowired
-    private ReviewRepository reviewRepository;
 
+    @Autowired
+    private ReviewRepository repository;
+
+    @Autowired
     private MongoTemplate mongoTemplate;
-    public Review createReview(String reviewBody, String imdbId){
-        Review review = reviewRepository.insert(new Review(reviewBody));
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = repository.insert(new Review(reviewBody));
 
         mongoTemplate.update(Movie.class)
                 .matching(Criteria.where("imdbId").is(imdbId))
-                .apply(new Update().push("reviewIds").value(review)).first();
+                .apply(new Update().push("reviews").value(review))
+                .first();
 
         return review;
-
     }
 }
