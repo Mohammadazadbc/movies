8364bf8fb61ff65e2baf7b8f721811ec9a5debf1
mohammad azad <mohammadazad.bc@gmail.com>
2023-11-15 10:45:33 +0100

    client side folder

--- movie-client/movie-gold-v1/src/App.js
+++ movie-client/movie-gold-v1/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [movies, setMovies] = useState();
+
+  const getMovice = async () => {
+    
+    try{
+      const response = await axios.get("http://localhost:8080/api/v1/movies");
+       console.log(response.data)
+       setMovies(response.data)
+      }
+      catch(err){
+        console.log(err)
+      }
+  }
+  
+  useEffect(()=>{
+      getMovice();
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
